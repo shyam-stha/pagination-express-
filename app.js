f6d73e8c06a4e077fe9c91db339949b46fb2a700
Shyam Stha <sthask2019@gmail.com>
2022-12-16 23:47:03 +0545

    Connect to mongodb database

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/pagination");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const postSchema = new mongoose.Schema({
+  name: String,
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Post = new mongoose.model("Post", postSchema);
+
+app.post("/", async (req, res) => {
+  try {
+    const post = new Post(req.body);
+    await post.save();
+    res.status(201).send("Scuccessfully created");
+  } catch (error) {
+    res.status(500).send("Error creating post");
+  }
+});
